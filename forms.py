7ab1df4407b535893d7a65c555c759d13966e640
Data Drops <mygreatbodyscan@gmail.com>
2018-02-24 22:42:22 +0530

    Final commit

---
+++ forms.py
@@ -0,0 +1,34 @@
+from flask_wtf import Form 
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+
+class SignupForm(Form):
+	first_name = StringField('First name',
+		                      validators=[DataRequired("Please enter your first name.")
+		                      ])
+	last_name = StringField('Last name',
+		                     validators=[DataRequired("Please enter your last name.")
+		                     ])
+	email = StringField('Email',
+		                validators=[DataRequired("Please enter your email."),
+		                            Email("Please enter a valid email address.")
+		                            ])
+	password = PasswordField('Password', validators=[
+		                    DataRequired("Please enter your password."),
+		                    Length(min=8,
+		                    message="Password must be of 8 characters or more.")
+		                                  ])
+	submit = SubmitField('Sign up')
+
+
+class LoginForm(Form):
+	email = StringField('Email',
+		    validators=[DataRequired("Please enter your email address."),
+		    Email("Please enter a valid email address.")])
+	password = PasswordField('Password',
+		    validators=[DataRequired("Please enter your password.")])
+	submit = SubmitField('Sign in')
+
+class  AddressForm(Form):
+	address = StringField('Address', validators=[DataRequired("Enter a address location.")])
+	submit = SubmitField("Search")
